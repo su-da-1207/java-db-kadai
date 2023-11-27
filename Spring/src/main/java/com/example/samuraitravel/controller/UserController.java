3ad3be7720cc7cd481ccec6464b21907cdf0171e
su-da-1207 <135832307+su-da-1207@users.noreply.github.com>
2023-11-27 21:15:39 +0900

    Add files via upload
    samuraitravel

---
+++ Spring/src/main/java/com/example/samuraitravel/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.samuraitravel.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.repository.UserRepository;
+import com.example.samuraitravel.security.UserDetailsImpl;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	private final UserRepository userRepository;
+	
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@GetMapping
+	public String index(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {
+		// UserDetailsImplオブジェクトからユーザーID取得し、リポジトリのgetReferenceById()メソッドに渡す
+		User user = userRepository.getReferenceById(userDetailsImpl.getUser().getId());
+		
+		model.addAttribute("user", user);
+		
+		return "user/index";
+	}
+}
